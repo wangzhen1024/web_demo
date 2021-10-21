f078834d6cfd3204b66dcd954da3c89194275334
wangzhen1024 <1039461479@qq.com>
2021-10-21 13:40:38 +0800

    222

--- src/main/java/com/example/web_demo/HelloServlet.java
+++ src/main/java/com/example/web_demo/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World! Hello,jenkins!111";
+        message = "Hello World! Hello,jenkins!222";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
